c94912296bb9bf396d0724002dac65fa6f791155
Micah McCallum <micahmccallum@outlook.com>
2020-04-09 00:28:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 class Song
 
-  extend Memorable
   extend Findable
-  extend Paramable::ClassMethods
-  include Paramable::InstanceMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
