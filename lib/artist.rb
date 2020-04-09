0fb0d9449e6a29aa54fccbacfb0eed4236cbee11
Micah McCallum <micahmccallum@outlook.com>
2020-04-09 00:27:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,8 +4,8 @@ class Artist
 
   extend Memorable
   extend Findable
-  extend Paramable::ClassMethods
-  include Paramable::InstanceMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
